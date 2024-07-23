8793e450a1d7931b28a1749d8486c3f6afe5a013
李青阳 <3131492575@qq.com>
2024-07-23 20:50:10 +0800

    dashboard UI 完成

--- dashboard/src/pages/index.vue
+++ dashboard/src/pages/index.vue
@@ -90,24 +90,53 @@ import { Menu as IconMenu, Message, Setting } from "@element-plus/icons-vue";
         </el-scrollbar>
       </el-aside>
       <el-main>
-        <el-row :gutter="20" :span="16">
-          <el-col :span="17">
-            <div class="main-title">我的一天</div>
-                <el-calendar>
-                  <template #date-cell="{ data }">
-                    <p :class="data.isSelected ? 'is-selected' : ''">
-                      {{ data.day.split("-").slice(2).join("-") }}
-                      {{ data.isSelected ? "✔️" : "" }}
-                    </p>
-                  </template>
-                </el-calendar>
+        <el-row :gutter="30" :span="16">
+          <el-col :span="16">
+            <el-card class="calendar">
+              <div class="main-title">我的一天</div>
+              <el-calendar>
+                <template #date-cell="{ data }">
+                  <p :class="data.isSelected ? 'is-selected' : ''">
+                    {{ data.day.split("-").slice(2).join("-") }}
+                    {{ data.isSelected ? "✔️" : "" }}
+                  </p>
+                </template>
+              </el-calendar>
+            </el-card>
           </el-col>
-          <el-col :span="7">
-            <div>test</div>
+          <el-col :span="8">
+            <el-card class="suggestion">
+              <template #header>
+                <div class="card-header">
+                  <span>建议</span>
+                </div>
+              </template>
+              <div>
+                <el-collapse v-model="activeNames" @change="handleChange">
+                  <el-collapse-item title="Consistency" name="1">
+                    <div>1</div>
+                  </el-collapse-item>
+                  <el-collapse-item title="Feedback" name="2">
+                    <div>2</div>
+                  </el-collapse-item>
+                  <el-collapse-item title="Efficiency" name="3">
+                    <div>3</div>
+                  </el-collapse-item>
+                  <el-collapse-item title="Controllability" name="4">
+                    <div>4</div>
+                  </el-collapse-item>
+                </el-collapse>
+              </div>
+            </el-card>
           </el-col>
         </el-row>
-        <el-row :gutter="20" :span="16">
-          <div>test2</div>
+        <el-row :gutter="30" :span="8">
+          <el-card class="data-stat">
+            text
+          </el-card>
+          <el-card class="hitoki">
+            text
+          </el-card>
         </el-row>
       </el-main>
     </el-container>
@@ -122,10 +151,46 @@ import { Menu as IconMenu, Message, Setting } from "@element-plus/icons-vue";
   border-right: none;
 }
 .el-calendar-day > p {
-  font-size: 15px;
+  font-size: 14px;
 }
-.animate__animated.animate__fadeInDown {
-  --animate-duration: 2s;
+:deep(.suggestion > .el-card__body) {
+  padding-top: 5px;
+}
+:deep(.el-collapse) {
+  border-top: none;
+}
+:deep(.el-collapse-item__header) {
+  border-bottom: none;
+}
+:deep(.el-collapse-item__wrap) {
+  border-bottom: none;
+}
+:deep(.el-collapse-item__content) {
+  padding-bottom: 10px;
+}
+:deep(.el-calendar__body) {
+  padding: 12px 20px 20px;
+}
+.calendar {
+  margin: 10px 0 0 20px;
+}
+.suggestion {
+  margin: 10px 20px auto auto;
+  width: 350px;
+  height: 540px;
+}
+.data-stat {
+  margin: 20px 0 0 35px;
+  width: 780px;
+  height: 200px;
+}
+.hitoki {
+  margin: 20px 0 0 45px;
+  width: 350px;
+  height: 200px;
+}
+.card-header {
+  font-size: 20px;
 }
 .main-title {
   margin: 5px auto;
@@ -134,6 +199,9 @@ import { Menu as IconMenu, Message, Setting } from "@element-plus/icons-vue";
   overflow: auto;
 }
 :deep(.el-calendar-table .el-calendar-day) {
-  height: 75px;
+  height: 65px;
+}
+.animate__animated.animate__fadeInDown {
+  --animate-duration: 2s;
 }
 </style>

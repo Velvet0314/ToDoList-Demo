915ffb54ed6c0c568b5b1d0dc4ea798e934d2a83
李青阳 <3131492575@qq.com>
2024-07-28 13:18:33 +0800

    具体任务的步骤展示

--- dashboard/src/components/plan.vue
+++ dashboard/src/components/plan.vue
@@ -1,18 +1,25 @@
 <script setup>
 import { Menu as IconMenu, Setting, Fold } from "@element-plus/icons-vue";
 import { Delete, Edit, Search, Share, Upload } from "@element-plus/icons-vue";
-import { ref, reactive, onMounted, inject, onUnmounted, computed } from "vue";
+import {
+  ref,
+  reactive,
+  onMounted,
+  inject,
+  onUnmounted,
+  computed,
+  watch,
+} from "vue";
 import { useLayoutStore } from "~/stores/layoutstore";
 import { storeToRefs } from "pinia";
 import { useTokenStore } from "~/stores/tokenstore";
-import { ta } from "element-plus/es/locale/index.mjs";
+import { ElDropdown, ElDropdownMenu, ElDropdownItem } from "element-plus";
 
 const format = (percentage) => (percentage === 100 ? "Full" : `${percentage}%`);
 
 const axios = inject("axios");
 const store = useLayoutStore();
 const { isCollapse } = storeToRefs(store);
-const selectedContent = ref(null); // 选中的任务
 
 const todayTasks = reactive([]);
 
@@ -37,18 +44,12 @@ onUnmounted(() => {
 });
 
 const showEditCard = ref(false); // 用于控制编辑卡片的显示
-const showDetailCard = ref(false);
-const tasks = reactive([]); // 用于存储所有任务
-
-const handleItemClick = (task) => {
-  showEditCard.value = true; // 显示编辑界面
-  selectedContent.value = task; // 设置当前选中的任务
-};
+const showDetailCard = ref(true);
 
 const addNewTask = async () => {
   showDetailCard.value = true;
-  showEditCard.value = true;
 
+  todayTasks.splice(0, todayTasks.length);
   // 获取token,通过验证
   const tokenStore = useTokenStore();
   const token = tokenStore.token;
@@ -69,8 +70,8 @@ const addNewTask = async () => {
         for (let i = 0; i < taskArray.length; i++) {
           todayTasks.push(taskArray[i]);
         }
-        console.log(todayTasks[0]); // 输出第一个任务
-        console.log(todayTasks[0].task_name); // 输出第一个任务的名称
+        // console.log(todayTasks[0]); // 输出第一个任务
+        // console.log(todayTasks[0].task_name); // 输出第一个任务的名称
       });
 
     // 获取当前新增的任务
@@ -79,6 +80,73 @@ const addNewTask = async () => {
   } catch (error) {}
 };
 
+const deteleTask = async (task, kind) => {
+  const tokenStore = useTokenStore();
+  const token = tokenStore.token;
+  console.log(token);
+  todayTasks.splice(0, todayTasks.length);
+  try {
+    // 发送删除请求到后端，这里假设后端提供了一个删除任务的API
+    await axios
+      .put(
+        `/task/deleteTask?id=${encodeURIComponent(task.id)}`,
+        {},
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      )
+      .then((response) => {
+        switch (kind) {
+          case 5:
+            {
+              axios
+                .get("/task/getTodayTask", {
+                  headers: {
+                    Authorization: token,
+                  },
+                })
+                .then((response) => {
+                  console.log(response.data.info);
+                  let taskArray = [];
+                  taskArray = response.data.info;
+                  for (let i = 0; i < taskArray.length; i++) {
+                    todayTasks.push(taskArray[i]);
+                  }
+                });
+            }
+            break;
+
+          case 4:
+            {
+            }
+            break;
+
+          case 3:
+            {
+            }
+            break;
+
+          case 2:
+            {
+            }
+            break;
+
+          case 1:
+            {
+            }
+            break;
+          default:
+            {
+            }
+            break;
+        }
+      });
+  } catch (error) {
+    console.error("Failed to delete task", error);
+  }
+};
 const handleClose1Click = () => {
   showDetailCard.value = false;
   showEditCard.value = false;
@@ -136,40 +204,41 @@ const thirdColSpan = computed(() => {
   return showEditCard.value ? 6 : 0;
 });
 
-const collapseData = reactive({
-  open: true, // 折叠栏是否打开
-  title: "新任务", // 折叠栏的标题
-  contents: [{ id: Date.now(), value: "" }], // 初始包含一个输入框
-});
+const selectedTask = ref(null); // 选中的任务
+const selectedContent = reactive([]); // 选中的任务
 
-// 点击下一步添加新的输入框
-const addNewInput = (task) => {
-  task.contents.push({ id: Date.now(), value: "" });
-};
+const taskDetail = async (task) => {
+  showEditCard.value = true; // 显示编辑界面
+  selectedTask.value = task; // 设置选中的任务
 
-const saveTitle = async () => {
+  const tokenStore = useTokenStore();
+  const token = tokenStore.token;
+  console.log(token);
+
+  console.log(task.id);
   try {
-    await axios.post("/save-title", { title: collapseData.title });
-    console.log("Title saved successfully");
-  } catch (error) {
-    console.error("Failed to save title", error);
-  }
-};
-
-const saveContent = async (content, task) => {
-  try {
-    await axios.post("/save-content", { title: task.title, content });
-    console.log("Content saved successfully");
-    task.detail = content; // 更新本地的详细内容
-  } catch (error) {
-    console.error("Failed to save content", error);
-  }
+    axios
+      .post(
+        "/step/getAllStep",
+        { task_id: task.id },
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+        selectedContent.values = response.data.info;
+        console.log(selectedContent.values);
+      });
+  } catch (error) {}
 };
 </script>
 
 <template>
   <el-row gutter="30" class="full-height">
-    <el-col :span="firstColSpan" class="full-height">
+    <el-col :span="8" class="full-height">
       <el-card
         :style="{ width: cardStyleList, height: '100%' }"
         class="full-height"
@@ -187,16 +256,16 @@ const saveContent = async (content, task) => {
               >
             </div>
             <el-collapse-item title="全部" name="1">
-              <div @click="handleItemClick(1)">1</div>
+              <div>1</div>
             </el-collapse-item>
             <el-collapse-item title="未完成" name="2">
-              <div @click="handleItemClick(2)">2</div>
+              <div>2</div>
             </el-collapse-item>
             <el-collapse-item title="已完成" name="3">
-              <div @click="handleItemClick(3)">3</div>
+              <div>3</div>
             </el-collapse-item>
             <el-collapse-item title="重要" name="4">
-              <div @click="handleItemClick(4)">4</div>
+              <div>4</div>
             </el-collapse-item>
           </el-collapse>
         </el-scrollbar>
@@ -204,36 +273,44 @@ const saveContent = async (content, task) => {
     </el-col>
 
     <!-- 详细内容 -->
-    <el-col :span="secondColSpan" class="full-height">
+    <el-col :span="8" class="full-height">
       <el-card
         v-if="showCard && showDetailCard"
         :style="{ width: cardStyleDetail, height: '100%' }"
+        style="max-height: 88vh; overflow: hidden"
       >
         <div class="button-wrapper">
           <el-button type="primary" round @click="handleClose1Click"
             >关闭</el-button
           >
         </div>
-        <div v-if="todayTasks.length === 0">
-          <el-empty description="暂无内容" />
-        </div>
-        <div
-          v-else
-          class="my-10"
-          v-for="task in todayTasks"
-        >
-          {{ task.task_name }}
-          <!-- 确保使用正确的属性来显示任务名称 -->
-        </div>
+        <el-scrollbar style="max-height: 86vh; overflow-y: auto">
+          <div
+            class="my-10 task-item"
+            v-for="task in todayTasks"
+            :key="task.id"
+          >
+            <el-dropdown trigger="contextmenu" placement="bottom-start">
+              <div class="w-[100%]" @click="taskDetail(task)">
+                {{ task.task_name }}
+              </div>
+              <template #dropdown>
+                <el-dropdown-menu>
+                  <el-dropdown-item>标记为重要</el-dropdown-item>
+                  <el-dropdown-item>标记为已完成</el-dropdown-item>
+                  <el-dropdown-item divided @click.native="deteleTask(task, 5)">
+                    删除任务
+                  </el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+            </el-dropdown>
+          </div>
+        </el-scrollbar>
       </el-card>
     </el-col>
 
     <!-- 新开的编辑页面 -->
-    <el-col
-      v-if="showCard && showEditCard"
-      :span="thirdColSpan"
-      class="full-height"
-    >
+    <el-col v-if="showCard && showEditCard" :span="8" class="full-height">
       <el-card style="max-height: 88vh; overflow: hidden">
         <el-scrollbar style="max-height: 86vh; overflow-y: auto">
           <el-collapse>
@@ -241,20 +318,19 @@ const saveContent = async (content, task) => {
               <template #title>
                 <el-input
                   placeholder="输入标题"
+                  v-model="selectedTask.task_name"
                   @blur="saveTitle"
                   size="small"
                   class="editable-title my-10"
                 />
               </template>
-              <div>
+              <div v-for="step in selectedContent.values"
+              :key="step.id">
                 <el-input
                   placeholder="输入内容"
-                  @blur="() => saveContent(item.value, selectedContent)"
+                  v-model="step.content"
                 />
               </div>
-              <el-button @click="() => addNewInput(selectedContent)"
-                >下一步</el-button
-              >
             </el-collapse-item>
           </el-collapse>
         </el-scrollbar>
@@ -264,6 +340,18 @@ const saveContent = async (content, task) => {
 </template>
 
 <style scoped>
+.task-item {
+  padding: 10px; /* 添加内边距使得布局更加美观 */
+  border-bottom: 1px solid #ccc; /* 添加分隔线 */
+  transition: background-color 0.3s; /* 平滑过渡效果 */
+  cursor: pointer;
+}
+
+.task-item:hover {
+  background-color: #cccccc; /* 悬浮时的背景颜色 */
+  cursor: pointer;
+}
+
 .full-height {
   height: 100%;
 }
@@ -272,7 +360,6 @@ const saveContent = async (content, task) => {
   display: flex;
   align-items: center;
   gap: 5%;
-  /* 调整输入框与按钮之间的间距 */
 }
 
 .button-wrapper {

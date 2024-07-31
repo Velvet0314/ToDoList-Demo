f50112b7ad1c63191db6ab9aab85375448d4cbc6
李青阳 <3131492575@qq.com>
2024-07-31 09:33:04 +0800

    packed

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,27 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import WindiCSS from 'vite-plugin-windicss'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import WindiCSS from "vite-plugin-windicss";
 
-import path from 'path'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   resolve: {
     alias: {
-      "~": path.resolve(__dirname,"src")
-    }
+      "~": path.resolve(__dirname, "src"),
+    },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://192.168.165.196:8080',
+      "/api": {
+        target: "http://192.168.165.196:8080",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/,''),
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
-  plugins: [
-    vue(),
-    WindiCSS(),
-  ],
-})
+  plugins: [vue(), WindiCSS()],
+});

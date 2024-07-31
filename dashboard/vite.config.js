aa8fc06829987bf7d215794fc1e9cbfc27ce0179
李青阳 <3131492575@qq.com>
2024-07-31 10:21:22 +0800

    re-build

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -7,6 +7,17 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0];
+          }
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
